c925df2aaf5153f0e9023e1373013fbbfaf5aa34
K V Sai Sri Chinmaye <kvsschinmaye1920@gmail.com>
2026-06-09 14:49:52 +0530

    Add for-each loop example

---
+++ src/04_Loops/04_ForEachLoop/NumExample.java
@@ -0,0 +1,13 @@
+public class NumExample {
+
+    public static void main(String[] args) {
+
+        int numbers[] = {17,35,63,91,49};
+
+         for(int num : numbers)
+         {
+             System.out.println(num);
+         }
+
+    }
+}
