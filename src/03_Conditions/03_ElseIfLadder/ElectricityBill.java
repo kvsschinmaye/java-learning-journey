e9ec8b731eefcb6d2d1cc061ff41a070f0aec795
K V Sai Sri Chinmaye <kvsschinmaye1920@gmail.com>
2026-06-10 11:44:01 +0530

    Update ElectricityBill.java and remove old .iml files

--- src/03_Conditions/03_ElseIfLadder/ElectricityBill.java
+++ src/03_Conditions/03_ElseIfLadder/ElectricityBill.java
@@ -20,6 +20,7 @@ public class ElectricityBill{
             bill = units * 1.5 ;
 
         }
+
         else if(units <= 200)
         {
             bill = (100 * 1.5) + (units - 100) *2.5;
@@ -29,10 +30,12 @@ public class ElectricityBill{
         {
             bill = (100 * 1.5) + (100 * 2.5) + (units -200) * 4.0;
         }
+
         else if (units <= 500)
         {
             bill = (100 * 1.5) + (100 * 2.5) + (100 * 4.0) + (units - 300) * 6.0;
         }
+
         else {
             bill = (100 * 1.5) + (100 * 2.5) + (100 * 4.0) + (200 * 6.0) + (units - 500) * 8.0;
         }
